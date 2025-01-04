7ea76c372627fc3ec32d109b96dc5681d99e1070
PvMDragonic <dragonicpvm@gmail.com>
2025-01-04 11:42:21 -0300

    Removing <SideMenu> from <OptionHelp>.
    It had been mistakenly added, as this button doesn't open a new
    interface.

--- src/components/OptionHelp/index.tsx
+++ src/components/OptionHelp/index.tsx
@@ -1,5 +1,3 @@
-import { useRef, useState } from "react";
-import { SideMenu } from "../SideMenu";
 import HelpIcon from "../../assets/HelpIcon";
 
 /**
@@ -10,35 +8,16 @@ import HelpIcon from "../../assets/HelpIcon";
  */
 export function OptionHelp(): JSX.Element
 {
-    const [showSideMenu, setShowSideMenu] = useState<boolean>(false);
-    const buttonRef = useRef<HTMLButtonElement>(null);
-
-    const buttonClass = `options-bar__button options-bar__button--${showSideMenu ? 'active' : 'inactive'}`;
-
     return (
-        <>
-            <SideMenu
-                buttonRef = {buttonRef}
-                showSideMenu = {showSideMenu}
-                setShowSideMenu = {setShowSideMenu}
-            >
-                {<></>}
-            </SideMenu>
-
-            <a 
-                href = "https://github.com/PvMDragonic/Wiki-Translator-2.0/blob/main/README.md" 
-                target = "_blank"
-                style = {{ textDecoration: 'none' }}
-            >        
-                <button 
-                    ref = {buttonRef}
-                    className = {buttonClass}
-                    onClick = {() => setShowSideMenu(prev => !prev)}
-                >
-                    <HelpIcon/>
-                    <span>AJUDA</span>
-                </button>
-            </a>
-        </>
+        <a 
+            href = "https://github.com/PvMDragonic/Wiki-Translator-2.0/blob/main/README.md" 
+            target = "_blank"
+            style = {{ textDecoration: 'none' }}
+        >        
+            <button className = "options-bar__button options-bar__button--inactive">
+                <HelpIcon/>
+                <span>AJUDA</span>
+            </button>
+    </a>
     )
 }
