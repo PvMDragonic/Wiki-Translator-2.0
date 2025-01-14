f66f46fbfa0c0ea1dcf62cca6bfda6906be21fc6
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2025-01-14 10:23:01 -0300

    <TextInput> translation refactor.
    - Moved all wiki related functions into a 'Wiki' Class;
    
    - Moved all translation related functions into the main 'translate()'
      function.

--- src/components/TextInput/translation.ts
+++ src/components/TextInput/translation.ts
@@ -1,206 +1,98 @@
-interface IWikiTemplates
-{
-    [key: string]: 
-    {
-        templateName: string,
-        templateParams: 
-        { 
-            [key: string]: string; 
-        },
-        templateValues: 
-        { 
-            [key: string]: 
-            { 
-                [key: string]: string; 
-            } 
-        }
-    }
-}
-
-async function requestWikiTemplates()
-{
-    const apiUrl = "https://pt.runescape.wiki/api.php";
-    const params = new URLSearchParams({
-        titles: "Usuário:PvM Dragonic/WikiTranslator.json",
-        prop: "revisions",
-        rvprop: "content",
-        action: "query",
-        format: "json"
-    });
-
-    return fetch(`${apiUrl}?${params}`)
-        .then(response => 
-        {
-            if (!response.ok) 
-                throw new Error(`HTTP error! Status: ${response.status}`);
-
-            return response.json();
-        })
-        .then(data => 
-        {
-            const queryPages = data.query.pages;
-            const page = queryPages[Object.keys(queryPages)[0]];
-            return JSON.parse(page.revisions[0]['*']);
-        })
-        .catch(error => 
-        {
-            console.error("Error fetching the module:", error);
-        });
-}
-
-async function requestWikiItemNames()
-{
-    const apiUrl = "https://pt.runescape.wiki/api.php";
-    const pages = ["data", "data/2", "data/3"];
-    const items: Record<string, string> = {};
-
-    for (const page of pages)
-    {
-        const params = new URLSearchParams({
-            titles: `Módulo:Traduções/${page}`,
-            prop: "revisions",
-            rvprop: "content",
-            action: "query",
-            format: "json"
-        });
-    
-        await fetch(`${apiUrl}?${params}`)
-            .then(response => 
-            {
-                if (!response.ok) 
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-    
-                return response.json();
-            })
-            .then(data => 
-            {
-                const queryPages = data.query.pages;
-                const pageData = queryPages[Object.keys(queryPages)[0]];
-                const content = pageData.revisions[0]['*'];
-                
-                content.split('] = {').slice(1).map((entry: string) => 
-                {
-                    const [_, en, pt] = entry.split('=');
-
-                    try
-                    {
-                        const key = en.match(/'(.*?[^\\])'/)![1]; // Handles "Nulodion's Notes". 
-                        const value = pt.split('\'')[1];
-                        items[key] = value;
-                    }
-                    catch(e)
-                    {
-                        // Some titles and stuff that contain HTML tags for coloring
-                        // are included in this, and they'll cause errors. Since the
-                        // main objetive is to translate items, may aswell ignore
-                        // titles and other edge cases for now.
-                    }
-                    
-                    
-                });
-            })
-            .catch(error => 
-            {
-                console.error("Error fetching the module:", error);
-            });
-    }
-
-    return items;
-}
-
-function extractInputData(text: string)
-{
-    // Some get here starting with '{{' and some do not.
-    const templateName = text.split('|')[0].replace(/^{{/, '').trim();
-    const templateEntries = text.split('\n').slice(1, -1).map(entry => 
-    {
-        const [key, value] = entry.split('=').map(
-            splitted => (splitted.startsWith('|') 
-                ? splitted.slice(1) 
-                : splitted
-            ).trim()
-        );
-
-        return { 
-            paramName: key,
-            paramValue: value
-        };
-    });
-
-    return {
-        templateName, 
-        templateEntries
-    };
-}
-
-function handleUnconventional(text: string, itemsNames: Record<string, string>, templateName: string)
-{
-    // Categories will fall here.
-    if (text.startsWith('[['))
-        return text.split('\n');
-    
-    // Navboxes will fall here.
-    if (!text.includes('|') && text !== '')
-        return [`{{${text}}}`]
-
-    const translatedParamName = itemsNames[text.split('|')[1]];
-
-    if (translatedParamName)
-        return [`{{${templateName}|${translatedParamName}}}`];
-
-    return text.split('\n');
-}
-
-function handleUpdateHistory(textLines: string[])
-{
-    function translateUpdateLine(textLine: string)
-    {
-        return `${textLine
-            .replace('UL', 'LA')
-            .replace('type=', 'tipo=')
-            .replace('update=', 'atualização=')
-            .replace('date=', 'data=')
-            .replace('=patch', '=correção')
-            .replace('=update', '=atualização')
-        }}}`; // All {{UL}} lose their closing brackets after the 'textToTranslate' split.
-    }
-
-    const updateHistory = textLines.flatMap((textLine, i) => 
-    {
-        if (i === 0) 
-        {
-            const [firstLine, secondLine] = textLine.split('\n');
-
-            return [
-                firstLine.startsWith('{') 
-                    // A single, clean {{UH}} input will already have brackets; else, it won't.
-                    ? firstLine.replace('UH', 'HA') 
-                    : `{{${firstLine.replace('UH', 'HA')}`, 
-                translateUpdateLine(secondLine)
-            ];
-        }
-
-        if (textLine.startsWith('*')) 
-        {
-            return textLine.split('\n').map(line => 
-                line.startsWith('* {') 
-                    ? translateUpdateLine(line) 
-                    : line
-            );
-        }
-
-        return []; // Effectively stops processing for any other cases.
-    });
-
-    // Need to add back the bracket removed from splitting 'textToTranslate'.
-    updateHistory[updateHistory.length - 1] = '}}';
-    return updateHistory;
-}
+import { Wiki } from "./wiki";
 
 export async function translate(textToTranslate: string)
 {
-    const templatesInfo = (await requestWikiTemplates()) as IWikiTemplates;
-    const itemsNames = await requestWikiItemNames();
+    function extractInputData(text: string)
+    {
+        // Some get here starting with '{{' and some do not.
+        const templateName = text.split('|')[0].replace(/^{{/, '').trim();
+        const templateEntries = text.split('\n').slice(1, -1).map(entry => 
+        {
+            const [key, value] = entry.split('=').map(
+                splitted => (splitted.startsWith('|') 
+                    ? splitted.slice(1) 
+                    : splitted
+                ).trim()
+            );
+
+            return { 
+                paramName: key,
+                paramValue: value
+            };
+        });
+
+        return {
+            templateName, 
+            templateEntries
+        };
+    }
+
+    function handleUnconventional(text: string, templateName: string)
+    {
+        // Categories will fall here.
+        if (text.startsWith('[['))
+            return text.split('\n');
+        
+        // Navboxes will fall here.
+        if (!text.includes('|') && text !== '')
+            return [`{{${text}}}`]
+
+        const translatedParamName = itemsNames[text.split('|')[1]];
+
+        if (translatedParamName)
+            return [`{{${templateName}|${translatedParamName}}}`];
+
+        return text.split('\n');
+    }
+
+    function handleUpdateHistory(textLines: string[])
+    {
+        function translateUpdateLine(textLine: string)
+        {
+            return `${textLine
+                .replace('UL', 'LA')
+                .replace('type=', 'tipo=')
+                .replace('update=', 'atualização=')
+                .replace('date=', 'data=')
+                .replace('=patch', '=correção')
+                .replace('=update', '=atualização')
+            }}}`; // All {{UL}} lose their closing brackets after the 'textToTranslate' split.
+        }
+
+        const updateHistory = textLines.flatMap((textLine, i) => 
+        {
+            if (i === 0) 
+            {
+                const [firstLine, secondLine] = textLine.split('\n');
+
+                return [
+                    firstLine.startsWith('{') 
+                        // A single, clean {{UH}} input will already have brackets; else, it won't.
+                        ? firstLine.replace('UH', 'HA') 
+                        : `{{${firstLine.replace('UH', 'HA')}`, 
+                    translateUpdateLine(secondLine)
+                ];
+            }
+
+            if (textLine.startsWith('*')) 
+            {
+                return textLine.split('\n').map(line => 
+                    line.startsWith('* {') 
+                        ? translateUpdateLine(line) 
+                        : line
+                );
+            }
+
+            return []; // Effectively stops processing for any other cases.
+        });
+
+        // Need to add back the bracket removed from splitting 'textToTranslate'.
+        updateHistory[updateHistory.length - 1] = '}}';
+        return updateHistory;
+    }
+
+    const templatesInfo = await Wiki.requestTemplates();
+    const itemsNames = await Wiki.requestItemNames();
 
     // Removes trailing newlines, then splits by newline double bracket ending with double bracket newline not followed by a pipe.
     const splitted = textToTranslate.replace(/\n+$/, '').split(/\n{{|}}\n(?!\|)/).filter(text => text !== '');
@@ -250,7 +142,7 @@ export async function translate(textToTranslate: string)
                 return text.split('\n');
 
             const _templateName = templateData ? templateData.templateName : templateName;
-            return handleUnconventional(text, itemsNames, _templateName);
+            return handleUnconventional(text, _templateName);
         }
 
         const translatedInput = templateEntries.map(entry =>
