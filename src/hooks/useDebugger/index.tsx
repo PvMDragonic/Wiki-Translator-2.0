44db90ee74e48eb57e18fe2eb1b2ae10b7441f12
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2025-01-18 14:03:06 -0300

    Code formatting.

--- src/hooks/useDebugger/index.tsx
+++ src/hooks/useDebugger/index.tsx
@@ -3,47 +3,45 @@ import SettingsContext from "../../pages/Home/settingsContext";
 
 export function useDebugger() 
 {
-  const 
-  {
-      retranslate,
-      setRetranslate,
-      debugging,
-      setDebugging,
-      debugSplitted,
-      setDebugSplitted,
-      debugTemplate,
-      setDebugTemplate,
-      debugSuccess,
-      setDebugSuccess,
-      debugSkipped,
-      setDebugSkipped,
-      debugMissing,
-      setDebugMissing,
-  } = useContext(SettingsContext);
-
-  useEffect(() => 
-      {
-      const settings = 
-      [
-          { key: "wikiTranslatorRetranslate", setter: setRetranslate },
-          { key: "wikiTranslatorDebugger", setter: setDebugging },
-          { key: "wikiTranslatorDebugSplitted", setter: setDebugSplitted },
-          { key: "wikiTranslatorDebugTemplate", setter: setDebugTemplate },
-          { key: "wikiTranslatorDebugSuccess", setter: setDebugSuccess },
-          { key: "wikiTranslatorDebugSkipped", setter: setDebugSkipped },
-          { key: "wikiTranslatorDebugMissing", setter: setDebugMissing },
-      ];
-
-      settings.forEach(({ key, setter }) => 
-          {
-          const value = localStorage.getItem(key);
-          if (value !== null) 
-          {
-            setter(JSON.parse(value));
-          }
-      });
+    const 
+    {
+        retranslate,
+        setRetranslate,
+        debugging,
+        setDebugging,
+        debugSplitted,
+        setDebugSplitted,
+        debugTemplate,
+        setDebugTemplate,
+        debugSuccess,
+        setDebugSuccess,
+        debugSkipped,
+        setDebugSkipped,
+        debugMissing,
+        setDebugMissing,
+    } = useContext(SettingsContext);
+  
+    useEffect(() => 
+    {
+        const settings = 
+        [
+            { key: "wikiTranslatorRetranslate", setter: setRetranslate },
+            { key: "wikiTranslatorDebugger", setter: setDebugging },
+            { key: "wikiTranslatorDebugSplitted", setter: setDebugSplitted },
+            { key: "wikiTranslatorDebugTemplate", setter: setDebugTemplate },
+            { key: "wikiTranslatorDebugSuccess", setter: setDebugSuccess },
+            { key: "wikiTranslatorDebugSkipped", setter: setDebugSkipped },
+            { key: "wikiTranslatorDebugMissing", setter: setDebugMissing },
+        ];
+  
+        settings.forEach(({ key, setter }) => 
+        {
+            const value = localStorage.getItem(key);
+            if (value !== null) 
+                setter(JSON.parse(value));
+        });
     }, [setRetranslate, setDebugging, setDebugSplitted, setDebugTemplate, setDebugSuccess, setDebugSkipped, setDebugMissing]);
-
+  
     return {
         retranslate,
         setRetranslate,
